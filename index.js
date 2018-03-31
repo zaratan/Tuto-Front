77db2b5d9f6f0668994de72eba757daeb2398435
Denis Zaratan Pasin <denis@pasin.fr>
2018-03-31 10:15:49 -0400

    Adding some JS

---
+++ index.js
@@ -0,0 +1,12 @@
+const users = [
+    { 'name': 'fred',   'age': 48 },
+    { 'name': 'barney', 'age': 34 },
+    { 'name': 'fred',   'age': 40 },
+    { 'name': 'barney', 'age': 36 },
+    { 'name': 'denis', 'age': 29 },
+    { 'name': 'denis', 'age': 30 },
+];
+
+const to_print = _.orderBy(users, ['name', 'age'], ['asc', 'desc']).map((e) => (`<li><span class="user-name">${e.name}</span>: ${e.age}</li>`)).join('')
+
+$('#container').append(`<ul>${to_print}</ul>`)
