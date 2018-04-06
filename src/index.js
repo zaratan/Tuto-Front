9b7624fe91f0feeae5c153e5c0e9ba511ed17ce2
Denis Zaratan Pasin <denis@pasin.fr>
2018-04-06 17:32:05 -0400

    The app is in React \o/

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import _ from 'lodash'
-import $ from 'jquery'
+import React from 'react'
+import { render } from 'react-dom'
 
 import './index.scss'
+import frustrationGif from './frustration.gif'
 
 const users = [
   { name: 'fred', age: 48 },
@@ -13,7 +15,27 @@ const users = [
 ]
 
 const to_print = _.orderBy(users, ['name', 'age'], ['asc', 'desc'])
-  .map(e => `<li><span class="user-name">${e.name}</span>: ${e.age}</li>`)
-  .join('')
 
-$('#container').append(`<ul>${to_print}</ul>`)
+render(
+  <div className="app">
+    <h1>my nice title</h1>
+    <div className="with-margin">
+      <p>Here goes the JS</p>
+      <div id="container">
+        <ul>
+          {to_print.map(e => (
+            <li>
+              <span className="user-name">{e.name}</span>: {e.age}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+    <div className="with-margin">
+      <img src={frustrationGif} alt="ECHEC" />
+      <div class="fru"></div>
+    </div>
+    <p>no color here</p>
+  </div>,
+  document.getElementById('app'),
+)
