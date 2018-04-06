d7633e05569ee4bd43c45938069445cca53f4199
Denis Zaratan Pasin <denis@pasin.fr>
2018-04-06 18:00:15 -0400

    Changing user-list into a real component

--- src/component/user-list.jsx
+++ src/component/user-list.jsx
@@ -3,19 +3,31 @@ import { orderBy } from 'lodash'
 
 import User from './user'
 
-const orderedUsers = users => orderBy(users, ['name', 'age'], ['asc', 'desc'])
+class UserList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
-export default ({ users }) => [
-  <ul key="list">
-    {orderedUsers(users).map(e => (
-      <User name={e.name} age={e.age} key={`${e.name}-${e.age}`} />
-    ))}
-  </ul>,
-  <form key="form">
-    <label htmlFor="name">Nom:</label>
-    <input type="text" name="name" />
-    <label htmlFor="age">Age:</label>
-    <input type="number" name="age" />
-    <input type="submit" />
-  </form>,
-]
+  orderedUsers() {
+    return orderBy(this.props.users, ['name', 'age'], ['asc', 'desc'])
+  }
+
+  render() {
+    return [
+      <ul key="list">
+        {this.orderedUsers().map(e => (
+          <User name={e.name} age={e.age} key={`${e.name}-${e.age}`} />
+        ))}
+      </ul>,
+      <form key="form">
+        <label htmlFor="name">Nom:</label>
+        <input type="text" name="name" />
+        <label htmlFor="age">Age:</label>
+        <input type="number" name="age" />
+        <input type="submit" />
+      </form>,
+    ]
+  }
+}
+
+export default UserList
