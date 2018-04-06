a95b70e36853048e6a808abbbdcaf93e9357408c
Denis Zaratan Pasin <denis@pasin.fr>
2018-04-06 17:56:37 -0400

    Adding form element

--- src/component/user-list.jsx
+++ src/component/user-list.jsx
@@ -5,10 +5,17 @@ import User from './user'
 
 const orderedUsers = users => orderBy(users, ['name', 'age'], ['asc', 'desc'])
 
-export default ({ users }) => (
-  <ul>
+export default ({ users }) => [
+  <ul key="list">
     {orderedUsers(users).map(e => (
       <User name={e.name} age={e.age} key={`${e.name}-${e.age}`} />
     ))}
-  </ul>
-)
+  </ul>,
+  <form key="form">
+    <label htmlFor="name">Nom:</label>
+    <input type="text" name="name" />
+    <label htmlFor="age">Age:</label>
+    <input type="number" name="age" />
+    <input type="submit" />
+  </form>,
+]
