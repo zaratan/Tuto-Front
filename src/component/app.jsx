a76d59ee858aa481fe013cf19da1971a18f813a4
Denis Zaratan Pasin <denis@pasin.fr>
2018-04-06 17:50:34 -0400

    Extracting margin-div

--- src/component/app.jsx
+++ src/component/app.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 
 import frustrationGif from '../frustration.gif'
 import UserList from './user-list'
+import MarginDiv from './margin-div'
 
 export default ({ users }) => (
   <div className="app">
     <h1>my nice title</h1>
-    <div className="with-margin">
+    <MarginDiv>
       <p>Here goes the JS</p>
       <div id="container">
         <UserList users={users} />
       </div>
-    </div>
-    <div className="with-margin">
+    </MarginDiv>
+    <MarginDiv>
       <img src={frustrationGif} alt="ECHEC" />
       <div class="fru"></div>
-    </div>
+    </MarginDiv>
     <p>no color here</p>
   </div>
 )
