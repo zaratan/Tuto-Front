dd9ea4f9e959ad98b7dd82e28069d53fbc40214f
Denis Zaratan Pasin <denis@pasin.fr>
2018-04-06 18:07:31 -0400

    Form is now monitor by state

--- src/component/user-list.jsx
+++ src/component/user-list.jsx
@@ -6,6 +6,24 @@ import User from './user'
 class UserList extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      users: this.props.users,
+      name: '',
+      age: 0,
+    }
+  }
+
+  handleChangeName(name) {
+    this.setState({
+      name,
+    })
+  }
+
+  handleChangeAge(age) {
+    this.setState({
+      age: age ? parseInt(age) : '',
+    })
   }
 
   orderedUsers() {
@@ -21,9 +39,23 @@ class UserList extends React.Component {
       </ul>,
       <form key="form">
         <label htmlFor="name">Nom:</label>
-        <input type="text" name="name" />
+        <input
+          type="text"
+          name="name"
+          value={this.state.name}
+          onChange={e => {
+            this.handleChangeName(e.target.value)
+          }}
+        />
         <label htmlFor="age">Age:</label>
-        <input type="number" name="age" />
+        <input
+          type="number"
+          name="age"
+          value={this.state.age}
+          onChange={e => {
+            this.handleChangeAge(e.target.value)
+          }}
+        />
         <input type="submit" />
       </form>,
     ]
